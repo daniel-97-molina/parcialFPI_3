5c22872c41d1948912ed4f108ba50be286afe730
andreacuellar2 <cuellarandrea@gmail.com>
2017-11-17 16:26:29 -0600

    Clase conector aun

--- src/main/java/com/example/demo/Conector.java
+++ src/main/java/com/example/demo/Conector.java
@@ -2,6 +2,7 @@ package com.example.demo;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -27,4 +28,16 @@ public void connect(){
 	            Logger.getLogger(Conector.class.getName()).log(Level.SEVERE, null, ex);
 	        }
 	 }
+ public void saveUsuario(String name, String email){
+        try {
+            PreparedStatement st = connect.prepareStatement("insert into users (name, email) values (?,?)");
+            st.setString(1, name);
+            st.setString(2, email);
+            st.execute();
+        } catch (SQLException ex) {
+            System.err.println(ex.getMessage());
+        }
+ 
+    }
+ 
 }
