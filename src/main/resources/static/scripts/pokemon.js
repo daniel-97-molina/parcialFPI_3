245434dbb850ac481505a32435347603d174ae5d
andreacuellar2 <cuellarandrea@gmail.com>
2017-11-19 19:40:38 -0600

    Evoluciones

--- src/main/resources/static/scripts/pokemon.js
+++ src/main/resources/static/scripts/pokemon.js
@@ -2,6 +2,10 @@ var pokemon;
 var urlBase = "https://pokeapi.co/api/v2/";
 var especie;
 var evolucion;
+var idEvoluciones = [];
+var evolutionArray = [];
+var cantidadEv = 0;
+
 
 function cargar(id) {
 	//limpiar controles
@@ -19,6 +23,10 @@ function cargar(id) {
 	get("#inicio").style.display = "none";
 	get("#layout-izquierda").style.display = "block";
 	get("#layout-derecha").style.display = "block";
+	idEvoluciones = [];
+	evolutionArray = [];
+	cantidadEv = 0;
+	
 	
 	var xhttp = new XMLHttpRequest();
 	xhttp.onreadystatechange = function() {
@@ -76,37 +84,138 @@ function cargarEspecie(id) {
 					break;
 				}
 			}
-			//cargarEvoluciones(especie.evolution_chain.url);
+			requestEvoluciones(especie.evolution_chain.url);
 		}
 	};
 	xhttp.open("GET", urlBase + "pokemon-species/" + id, true);
 	xhttp.send();
 }
-
-function cargarEvoluciones(path) {
+/////////////evoluciones///////////////////////
+function requestEvoluciones(urlevolucion){
 	var xhttp = new XMLHttpRequest();
-	xhttp.onreadystatechange = function() {
-		if (this.readyState == 4 && this.status == 200) {
-			evolucion = JSON.parse(this.responseText);
-			console.log(evolucion);
-			if (especie.habitat != null && especie.habitat != "undefined") {
-				$("#specie").innerHTML = especie.habitat.name;
-			} else {
-				$("#specie").innerHTML = "Desconocido";
-			}
+	xhttp.onreadystatechange=function(){
+	if (this.readyState == 4 && this.status == 200) {
+	   evolucion = JSON.parse(this.responseText);
+	   cargarEvoluciones(evolucion);
+	}
+};
+xhttp.open("GET", urlevolucion, true)
+xhttp.send();
+}
 
-			// Para la generacion
-			$("#generacion").innerHTML = especie.generation.name;
+function cargarEvoluciones(evolucion){
+var tercera = 0;
+var areaDivEvoluciones = document.getElementById('divEvoluciones');
+areaDivEvoluciones.innerHTML = "";
+evolutionArray = evolucion.chain;
+cantidadEv = evolutionArray.evolves_to.length;
+for (var i = -1; i < cantidadEv; i++) {
+  if (i<0) {
+    idEvoluciones.push(evolucion.chain.species.url.slice(42, -1));
+    EvolucionURL(parseInt(idEvoluciones[0]), areaDivEvoluciones, evolutionArray.species.name);
 
-			// Para la evolucion 1
-			$("#evolucion1").innerHTML = evolucion.chain.species.name;
-			aEvoluciones[0] = evolucion.chain.species.url.slice(42, -1);
-			iContador = 1;
-			console.log("urlEvolucion 1:  " + aEvoluciones[0]);
+  }else{
+    idEvoluciones.push(evolucion.chain.evolves_to[i].species.url.slice(42, -1));
+    EvolucionURL(parseInt(idEvoluciones[i+1]), areaDivEvoluciones, evolucion.chain.evolves_to[i].species.name);
+    tercera = idEvoluciones.length;
+  }
+}
+for (var i = 0; i < evolutionArray.evolves_to.length; i++) {
+  for (var j = 0; j < evolutionArray.evolves_to[i].evolves_to.length; j++) {
+    idEvoluciones.push(evolucion.chain.evolves_to[i].evolves_to[j].species.url.slice(42, -1));
+    EvolucionURL(parseInt(idEvoluciones[tercera]), areaDivEvoluciones, evolutionArray.evolves_to[i].evolves_to[j].species.name);
+  }
+}
+}
+
+//////////////El metodo que esta adentro///////////////////////////////////////////////////////////////
+
+function EvolucionURL(idDeseado, areaDivEvoluciones, nombre){
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange=function() {
+  if (this.readyState == 4 && this.status == 200) {
+    pokemon = JSON.parse(this.responseText);
+      GenerarDivEvolucion(areaDivEvoluciones, pokemon.sprites.front_default, nombre,idDeseado);
+  }
+  };
+  xhttp.open("GET", "https://pokeapi.co/api/v2/pokemon/"+idDeseado, true);
+  xhttp.send();
+}
+
+/////////////////// Metodo que crea los divs//////////////////////////////////////////////////////////////////
+
+function GenerarDivEvolucion(divPrincipal, ruta, nombre, idDeseado) {
+var divEvolucion = document.createElement("DIV");
+var divEvolucionImg = document.createElement("DIV");
+var divEvolucionName = document.createElement("DIV");
+var hTitulo = document.createElement("H1");
+var iImg = document.createElement("IMG");
+var eName = document.createTextNode(nombre);
+iImg.setAttribute("src",ruta);
+divEvolucion.setAttribute("onclick", "nuevoPokemon("+idDeseado+")")
+hTitulo.appendChild(eName);
+divEvolucionImg.appendChild(iImg);
+divEvolucionName.appendChild(hTitulo);
+divEvolucion.appendChild(divEvolucionImg);
+divEvolucion.appendChild(divEvolucionName);
+divPrincipal.appendChild(divEvolucion);
+}
+
+var pokecomply = new Array();
+window.onload = function() {
+var xmlhttp = new XMLHttpRequest();
+xmlhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+        var myObj = JSON.parse(this.responseText);
+        for(i=0 ; i<802 ; i++){
+        	pokecomply[i] = String(myObj.results[i].name);
+        }
+        creadorDeListas(pokecomply);
+    }
+};
+xmlhttp.open("GET", "https://pokeapi.co/api/v2/pokemon/?limit=802" , true);
+xmlhttp.send();
+}
+
+function creadorDeListas(pokecomply){
+for(i=0; i<802 ; i++){
+  var node = document.createElement("OPTION");
+  var textnode = document.createTextNode(pokecomply[i]);
+  node.appendChild(textnode);
+  node.setAttribute("id",i);
+  node.setAttribute("name", pokecomply[i]);
+  document.getElementById("lista").appendChild(node);
+}
+}
+
+function llenarPokemon(e) {
+	if(e.keyCode == 13){
+		buscador = document.getElementById("txtBusqueda").value
+	if (Existe(pokecomply, buscador)){
+		var optionS = document.getElementsByName(buscador);
+		var pokeIdS = optionS[0].getAttribute("id");
+		var pokeId = parseInt(pokeIdS)+1;
+		cargar(pokeId);
 		}
-	};
-	xhttp.open("GET", path, true);
-	xhttp.send();
+	else{
+		alert("Busqueda no válida")
+	}
+	}
+	
+}
+
+function Existe(array, texto){
+	 var tamanio = array.length;
+	  for (var i = 0; i < tamanio; i++) {
+	    if (array[i] == texto) {
+	      return true;
+	    }
+	  }
+	  return false;
+}
+
+function nuevoPokemon(requestID){
+	cargar(requestID);	
 }
 
 function get(objeto) {
