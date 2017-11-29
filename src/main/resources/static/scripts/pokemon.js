5bd39d30ab13dfc730a2d24b4f075d4f8caf11d0
Daniel Molina <daniel.97.molin@gmail.com>
2017-11-29 04:23:24 -0600

    final

--- src/main/resources/static/scripts/pokemon.js
+++ src/main/resources/static/scripts/pokemon.js
@@ -379,11 +379,10 @@ get("#btnFavorito").onclick = function() {
       }else{
     	  alert("debe registrarse");
       }
-    };
-}
+    }
+};
 
 function cerrarSesion(){
-	alert("cerrando");
 	document.cookie = "usuarioLogueado=;expires=Thu, 01 Jan 1970 00:00:01 GMT;";
 }
     
